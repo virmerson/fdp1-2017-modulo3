96665672c4662f4158a220fffda9f7f342bd40f7
virmerson <virmerson@hightechcursos.com.br>
2017-06-24 13:54:00 -0400

    Aula 03

--- src/main/webapp/js/autenticacao.js
+++ src/main/webapp/js/autenticacao.js
@@ -1,22 +1,27 @@
 //Classe
-Autenticacao = function(){}
+Autenticacao = function(){
 
-//Método
-Autenticacao.prototype.validarCampos= function (){
 
-    elementoUsuario = document.getElementById("txtUsuario");
-    textoDigitadoUsuario = elementoUsuario.value;
-    
-    elementoSenha =  document.getElementById("txtSenha");
-    textoDigitadoSenha = elementoSenha.value;
-    
-    if (textoDigitadoUsuario=="" || textoDigitadoSenha=="" ){
-        window.alert("#Os campos usuário e senha devem ser digitados!#");
-        return false;
-    } else{
-    
-        return true;
+    //Método
+    this.validarCampos= function (){
+
+        elementoUsuario = document.getElementById("txtUsuario");
+        textoDigitadoUsuario = elementoUsuario.value;
+        
+        elementoSenha =  document.getElementById("txtSenha");
+        textoDigitadoSenha = elementoSenha.value;
+        
+        if (textoDigitadoUsuario=="" || textoDigitadoSenha=="" ){
+            window.alert("#Os campos usuário e senha devem ser digitados!#");
+            return false;
+        } else{
+        
+            return true;
+        }
     }
+
 }
+
+
 //Instanciando objeto
 aut  = new Autenticacao();
